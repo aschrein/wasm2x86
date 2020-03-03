0746f9878301b2e6d4a8f5c5e9de7d9a7ef93e6f
Anton Schreiner <antonschrein@gmail.com>
2020-03-03 10:12:55 +0000

    Add binary module dump

--- main.cpp
+++ main.cpp
@@ -5,8 +5,9 @@
 #include "src/execution/isolate.h"
 #include "src/wasm/wasm-engine.h"
 #include "src/wasm/wasm-module.h"
-#include "src/wasm/wasm-objects.h"
 #include "src/wasm/wasm-objects-inl.h"
+#include "src/wasm/wasm-objects.h"
+#include <iomanip>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -28,7 +29,12 @@ std::vector<v8::internal::byte> readFile(const char *filename) {
 int main(int argc, char *argv[]) {
   using namespace v8::internal::wasm;
   using namespace v8::internal;
-  FLAG_experimental_wasm_simd = true;
+  i::FLAG_experimental_wasm_simd = true;
+  i::FLAG_perf_prof_annotate_wasm = true;
+  i::FLAG_code_comments = true;
+  i::FLAG_opt = true;
+  i::FLAG_wasm_opt = true;
+  i::FLAG_wasm_stack_checks = false;
   v8::V8::InitializeICUDefaultLocation(argv[0]);
   v8::V8::InitializeExternalStartupData(argv[0]);
   std::unique_ptr<v8::Platform> platform(v8::platform::NewDefaultPlatform());
@@ -87,9 +93,49 @@ int main(int argc, char *argv[]) {
     WasmCodeRefScope code_ref_scope;
     WasmCode *code = native_module->AddCompiledCode(std::move(result));
     DCHECK_NOT_NULL(code);
-    code->Print();
+    // code->Print();
   }
-
+  {
+    std::ofstream out("module.bin", std::ios::out | std::ofstream::binary);
+    // execute to get hex dump
+    // od -A n -t x1 module.bin
+    // or to get disassembly
+    // objdump -M intel -D -b binary -mi386 -Maddr16,data16 module.bin 
+    auto print = [&out](byte const *origin, size_t size) {
+      // byte const *ptr = origin;
+      // byte const *end = origin + size;
+      // uint64_t dbg_cnt = 1 << 30;
+      // while(ptr != end &&
+      //   (dbg_cnt-->0)) {
+      //     std::cout << std::internal << std::setfill('0') << std::setw(2) <<
+      //     std::hex << (int)(*ptr++) << " ";
+      // }
+      // std::cout << "\n";
+      out.write((char const *)origin, size);
+    };
+    NativeModule *native_module = module_obj->native_module();
+    auto wasm_codes = native_module->SnapshotCodeTable();
+    for (WasmCode *wasm_code : wasm_codes) {
+      if (wasm_code->kind() != WasmCode::Kind::kFunction)
+        continue;
+      auto const &instructions = wasm_code->instructions();
+      int instruction_size = wasm_code->unpadded_binary_size();
+      if (wasm_code->constant_pool_offset() < instruction_size) {
+        instruction_size = wasm_code->constant_pool_offset();
+      }
+      if (wasm_code->safepoint_table_offset() &&
+          wasm_code->safepoint_table_offset() < instruction_size) {
+        instruction_size = wasm_code->safepoint_table_offset();
+      }
+      if (wasm_code->handler_table_offset() < instruction_size) {
+        instruction_size = wasm_code->handler_table_offset();
+      }
+      print(&instructions[0], instruction_size);
+      byte nops[] = {0x90, 0x90, 0x90, 0x90, 0x90,
+                     0x90, 0x90, 0x90, 0x90, 0x90};
+      print(nops, sizeof nops);
+    }
+  }
   v8::V8::Dispose();
   v8::V8::ShutdownPlatform();
   return 0;
